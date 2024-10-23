f4655aa64922d7114cf64d6374236ff078939a8f
Micael Silva <374220+micaelsilva@users.noreply.github.com>
2024-10-23 13:10:27 -0300

    Add air conditioner as a device

--- thinq2/model/device/__init__.py
+++ thinq2/model/device/__init__.py
@@ -1,7 +1,9 @@
 from .base import Device
 from .laundry import LaundryDevice
+from .airconditioner import AirConditionerDevice
 
 device_types = {
     201: LaundryDevice,
     202: LaundryDevice,
+    401: AirConditionerDevice,
 }
---
+++ thinq2/model/device/airconditioner.py
@@ -0,0 +1,12 @@
+from dataclasses import field
+from marshmallow_dataclass import dataclass
+
+from thinq2.schema import CamelCaseSchema
+
+from .base import Device
+
+
+@dataclass(base_schema=CamelCaseSchema)
+class AirConditionerDevice(Device):
+    state = 0
+    operation: int = field(metadata=dict(data_key="airState.operation"))
